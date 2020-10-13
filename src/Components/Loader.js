d09d1d6f77634acf686220438633603ae4354309
Yuna Lee <carnivalss@naver.com>
2020-10-13 14:03:24 +0900

    Style - Loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -3,41 +3,30 @@ import styled, { keyframes } from "styled-components";
 
 const ani__circle = keyframes`
 	0% {
-		border-radius: 0 0 0 0;
-		background-color: #00E7C2;
-		transform: rotate(0deg);
+		background-color: #wheat;
 	}
 	25% {
-		border-radius: 50% 0 0 0;
-		background-color: #c9c9c9;
-		transform: rotate(45deg);
+		background-color: #CD853E;
 	}
 	50% {
-		border-radius: 50% 50% 0 0;
 		background-color: #F4D300;
-		transform: rotate(90deg);
 	}
 	75% {
-		border-radius: 50% 50% 50% 0;
-		background-color: #00E7C2;
-		transform: rotate(135deg);
+		background-color: #wheat;
 	}
 	100% {
-		border-radius: 50% 50% 50% 50%;
-		background-color: #c9c9c9;
-		transform: rotate(180deg);
+		background-color: #CD853E;
 	}
 `;
 
 const Circle = styled.div`
-  width: 110px;
-  height: 110px;
+  width: 100%;
+  height: 100%;
   background-color: ${(props) => props.theme.pointHoverColor};
 	animation: ${ani__circle} 1s infinite alternate;
 	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
+	top: 0;
+	left: 0;
 	z-index: 9999;
 `;
 
