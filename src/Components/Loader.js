8cc36df17642f1bfeef19cc24adb847891be9992
Yuna Lee <carnivalss@naver.com>
2020-10-13 14:25:59 +0900

    Style - Loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -3,19 +3,19 @@ import styled, { keyframes } from "styled-components";
 
 const ani__circle = keyframes`
 	0% {
-		background-color: yellowgreen;
+		background-color: wheat;
 	}
 	25% {
-		background-color: darkseagreen;
+		background-color: burlywood;
 	}
 	50% {
-		background-color: seagreen;
+		background-color: peru;
 	}
 	75% {
-		background-color: yellowgreen;
+		background-color: wheat;
 	}
 	100% {
-		background-color: darkseagreen;
+		background-color: burlywood;
 	}
 `;
 
@@ -23,7 +23,7 @@ const Circle = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${(props) => props.theme.pointColor};
-	animation: ${ani__circle} 1s infinite alternate;
+	animation: ${ani__circle} 2s infinite alternate;
 	position: absolute;
 	top: 0;
 	left: 0;
